7a5b326153fda8e59058fe2547bbe287c272f89d
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-09-13 21:48:43 +0900

    esm 도 적용

--- PointerEventHandler.esm.js
+++ PointerEventHandler.esm.js
@@ -1,10 +1,8 @@
 export default class PointerEventHandler{
     pointers = null
     maxActivePointers = 0;
-    target = null; 
+    target = null;
     downAt = null //down 이벤트 발생시간
-
-    inputMode = 'all'; //all, pen, mouse, touch
 
     constructor(target){
         // this.editor = editor
@@ -33,50 +31,123 @@ export default class PointerEventHandler{
         this.target.removeEventListener('pointerleave',this._pointerend)
         this.target.removeEventListener('pointercancel',this._pointerend)
     }
-    
-     extractPointerData({ pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y }) {
+    getCustomPointerEventDetail(data){
+        return {pointerEventHandler:this, ...data};
+    }
+    getCustomPointerEvent(type,{ bubbles = false, cancelable = false, composed = false, detail = null } = {}){
+        return new CustomEvent(type, { bubbles,cancelable,composed,detail });
+    }
+    extractPointerData({ pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y }) {
         return { pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y };
     }
     pointerdown = (event)=>{
         if(this.pointers.size===0){ this.downAt = Date.now(); this.maxActivePointers=0; }
+        const pointerData = this.extractPointerData(event)
         if(!this.pointers.has(event.pointerId)){
             this.target.setPointerCapture(event.pointerId);
-            this.pointers.set(event.pointerId, this.extractPointerData(event));
-            this.maxActivePointers = Math.max(this.maxActivePointers,this.pointers.size)       
+            this.pointers.set(event.pointerId, pointerData);
+            this.maxActivePointers = Math.max(this.maxActivePointers,this.pointers.size)
         }
-        
+
         this.onpointerdown?.(event)
+        // 커스텀 이벤트 발생
+        const detail = this.getCustomPointerEventDetail({pointerData})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+        // 멀티 포인터 동작
+        console.log(this.pointers.size);
+        
+        if(this.pointers.size>1){ this.multipointerdown(event); }
     }
     pointermove = (event)=>{
         if (!this.downAt || !this.pointers.has(event.pointerId)) return;
-        this.pointers.set(event.pointerId, this.extractPointerData(event));
+        const pointerData = this.extractPointerData(event)
+        this.pointers.set(event.pointerId, pointerData);
         this.onpointermove?.(event);
+        // 커스텀 이벤트 발생
+        const detail = this.getCustomPointerEventDetail({pointerData})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+        if(this.pointers.size>1){ this.multipointermove(event); } // 멀티 포인터 동작
     }
     _pointerend = (event)=>{
-        if (this.pointers.delete(event.pointerId)) {
-            if (event.type === 'pointerup') this.onpointerup?.(event);
-            else if (event.type === 'pointerleave') this.onpointerleave?.(event);
-            else if (event.type === 'pointercancel') this.onpointercancel?.(event);
+        const pointerData = this.extractPointerData(event)
+        if(this.pointers.has(event.pointerId)){
+            if (event.type === 'pointerup'){
+                this.onpointerup?.(event);
+                // 커스텀 이벤트 발생
+                const detail = this.getCustomPointerEventDetail({pointerData})
+                this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+                if(this.pointers.size>1){ this.multipointerup(event); } // 멀티 포인터 동작
+            }
+            else if (event.type === 'pointerleave'){
+                this.onpointerleave?.(event);
+                // 커스텀 이벤트 발생
+                const detail = this.getCustomPointerEventDetail({pointerData})
+                this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+                if(this.pointers.size>1){ this.multipointerleave(event); } // 멀티 포인터 동작
+            }
+            else if (event.type === 'pointercancel'){
+                this.onpointercancel?.(event);
+                // 커스텀 이벤트 발생
+                const detail = this.getCustomPointerEventDetail({pointerData})
+                this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+                if(this.pointers.size>1){ this.multipointercancel(event); } // 멀티 포인터 동작
+            }
+            this.pointers.delete(event.pointerId)
         }
         this.target.releasePointerCapture(event.pointerId);
         if(this.pointers.size===0){this.downAt = null; this.maxActivePointers=0;}
     }
-    
+
 
     onpointerdown(event){
-        console.log('onpointerdown');
+        console.debug('onpointerdown');
     };
     onpointermove(event){
-        console.log('onpointermove');
+        console.debug('onpointermove');
     };
     onpointerup(event){
-        console.log('onpointerup');
+        console.debug('onpointerup');
     };
     onpointerleave(event){
-        console.log('onpointerleave');
+        console.debug('onpointerleave');
     };
     onpointercancel(event){
-        console.log('onpointercancel');
+        console.debug('onpointercancel');
     };
+
+
+    // TODO;
+    // 멀티 포인터 처리부
+    multipointerdown(event){
+        return this._multipointerEvent(event);
+    };
+    multipointermove(event){
+        return this._multipointerEvent(event);
+    };
+    multipointerup(event){
+        return this._multipointerEvent(event);
+    };
+    multipointerleave(event){
+        return this._multipointerEvent(event);
+    };
+    multipointercancel(event){
+        return this._multipointerEvent(event);
+    };
+    _multipointerEvent(event){
+        const pointerData = this.pointers.get(event.pointerId)??this.extractPointerData(event);
+        
+        //-- 멀티포인터 데이터 계산
+        const pointersValues = Array.from(this.pointers.values());
+        const multiPointerData = {
+            centerX:pointersValues.reduce((sum, p) => sum + p.clientX, 0) / pointersValues.length,
+            centerY:pointersValues.reduce((sum, p) => sum + p.clientY, 0) / pointersValues.length,
+        }
+        
+        const detail = this.getCustomPointerEventDetail({pointerData,multiPointerData})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`multi${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));
+        if(this.pointers.size==2 && this.maxActivePointers==2){
+            this.target.dispatchEvent(this.getCustomPointerEvent(`two${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail }));    
+        }
+    }
 }
 
