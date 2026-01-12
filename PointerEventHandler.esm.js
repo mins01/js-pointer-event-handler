6785e43e8bf7b8b5b4fd75669a733e966f42aece
mins01 <mins01@inuscomm.co.kr>
2026-01-12 14:21:16 +0900

    performance.now() 사용

--- PointerEventHandler.esm.js
+++ PointerEventHandler.esm.js
@@ -1,11 +1,20 @@
 export default class PointerEventHandler{
+    static VERSION = '2026.01.12'; // 버전 날짜.
+
     pointers = null
     maxActivePointers = 0;
     target = null;
     downAt = null //down 이벤트 발생시간
+    static interactionThreshold = {
+        mouse: 4,
+        pen: 5,
+        touch: 8,
+    }; // 요소 클릭 후 드래그 캡처 시작 임계값(px)
+    // interactionThreshold = null; //요소 클릭 후 드래그 캡처 시작 임계값(px)
 
     constructor(target){
-        // this.editor = editor
+        this.interactionThreshold = { ... this.constructor.interactionThreshold }
+        
         this.pointers = new Map(); // 멀티터치 제어용
         this.maxActivePointers = 0;
         this.downAt = null;
@@ -13,23 +22,32 @@ export default class PointerEventHandler{
             this.addEventListener(target);
         }
     }
+    addEventListenerOptions = { passive: false }
     addEventListener(target){
         this.target = target
-        const options = { passive: false };
-        target.addEventListener('pointerdown',this.pointerdown);
-        target.addEventListener('pointermove',this.pointermove,options)
-        target.addEventListener('pointerup',this._pointerend)
-        target.addEventListener('pointerleave',this._pointerend); // 거의 발생하지 않을꺼다
-        target.addEventListener('pointercancel',this._pointerend) // 시스템 메세지 등이 뜨면 발생.
+        // const options = { passive: false };
+        this.target.addEventListener('pointerdown',this.pointerdown);
+        this.target.addEventListener('pointermove',this.pointermove,this.addEventListenerOptions)
+        
+        this.target.addEventListener('pointerup',this._pointerend)
+        // this.target.addEventListener('pointerleave',this._pointerend); // 거의 발생하지 않을꺼다. 발생시 문제가 될 수 있어서 체크 안한다.
+        this.target.addEventListener('pointercancel',this._pointerend) // 시스템 메세지 등이 뜨면 발생.
+        
+        this.target.addEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡처
+        this.target.addEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡처
     }
     removeEventListener(){
         if(!this.target) {return; }
-        const options = { passive: false };
+        // const options = { passive: false };
         this.target.removeEventListener('pointerdown',this.pointerdown);
-        this.target.removeEventListener('pointermove',this.pointermove,options)
+        this.target.removeEventListener('pointermove',this.pointermove,this.addEventListenerOptions)
+
         this.target.removeEventListener('pointerup',this._pointerend)
-        this.target.removeEventListener('pointerleave',this._pointerend)
+        // this.target.removeEventListener('pointerleave',this._pointerend)
         this.target.removeEventListener('pointercancel',this._pointerend)
+        
+        this.target.removeEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡처
+        this.target.removeEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡처
     }
     getCustomPointerEventDetail(data){
         return {pointerEventHandler:this, ...data};
@@ -38,12 +56,28 @@ export default class PointerEventHandler{
         return new CustomEvent(type, { bubbles,cancelable,composed,detail });
     }
     extractPointer({ pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y }) {
-        return { pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y };
+        // timeStamp 값 신뢰하지 마라. 브라우저마다 기준값이 다를 수 있다!!!! time: performance.now() 를 대신 사용함.
+        return { pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp,time: performance.now(), tangentialPressure,x,y };
+    }
+
+    get isPointerCaptured(){
+        return this.target.hasPointerCapture(this.firstPointer?.pointerId)
+    }
+    gotpointercapture = (event)=>{
+        // this.isPointerCaptured = true;
+        const detail = this.getCustomPointerEventDetail({originalEvent:event})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
+    }
+    lostpointercapture = (event)=>{
+        // this.isPointerCaptured = false;
+        this._pointerend(event)
     }
 
     firstPointer = null
     prevPointer = null
     pointerdown = (event)=>{
+        // console.log(event.target,event.currentTarget);
+        
         const pointer = this.extractPointer(event)
         if(this.pointers.size===0){
             this.downAt = Date.now();
@@ -52,7 +86,7 @@ export default class PointerEventHandler{
             this.prevPointer = pointer;
         }
         if(!this.pointers.has(event.pointerId)){
-            this.target.setPointerCapture(event.pointerId);
+            if(event.target === this.target){ this.target.setPointerCapture(event.pointerId); } // 이벤트 발생 요소가 이벤트 처리 요소가 같은 경우만 pointerdown 에서 캡처한다.
             this.pointers.set(event.pointerId, pointer);
             this.maxActivePointers = Math.max(this.maxActivePointers,this.pointers.size)
         }
@@ -65,7 +99,13 @@ export default class PointerEventHandler{
         const pointer = this.extractPointer(event)
         this.pointers.set(event.pointerId, pointer);
         this.onpointermove?.(event);
-        this._pointerEvent(event,pointer) // 포인터 이벤트 처리
+        const detail  = this._pointerEvent(event,pointer) // 포인터 이벤트 처리
+
+        // 캡처를 안했고 드래그 이동이 4~8px 이 있을 경우만 캡처한다.
+        if(!this.target.hasPointerCapture(event.pointerId) && Math.abs(detail.totalMetrics.distance) >= (this.interactionThreshold[event.pointerType]??8)){
+            this.target.setPointerCapture(event.pointerId); 
+        }
+
         if(event.isPrimary){
             this.prevPointer = pointer;
         }
@@ -75,10 +115,11 @@ export default class PointerEventHandler{
         if(this.pointers.has(event.pointerId)){
             this?.[`on${event.type}`]?.(event);
             this._pointerEvent(event,pointer) // 포인터 이벤트 처리
-
+            this._pointerEvent(event,pointer,'pointerend') // 포인터 end 이벤트 처리
+            if(this.target.hasPointerCapture(event.pointerId)) this.target.releasePointerCapture(event.pointerId);
             this.pointers.delete(event.pointerId)
         }
-        this.target.releasePointerCapture(event.pointerId);
+        
         if(this.pointers.size===0){
             this.downAt = null;
             this.maxActivePointers=0;
@@ -99,7 +140,8 @@ export default class PointerEventHandler{
         // ----------------------
         // 시간 간격
         // ----------------------
-        const time = (pointer2.timeStamp - pointer1.timeStamp) / 1000 ;
+        // const time = (pointer2.timeStamp - pointer1.timeStamp) / 1000 ;
+        const time = (pointer2.time - pointer1.time) ;
 
         // ----------------------
         // 순간 속도 (프레임 기준)
@@ -138,16 +180,18 @@ export default class PointerEventHandler{
             velocityVector,
         };
     }
-    _pointerEvent(event,pointer){
+    _pointerEvent(event,pointer,eventType=event.type){
         const firstPointer = this.firstPointer;
         const prevPointer = this.prevPointer;
         const metrics = this.getPointerMetrics(prevPointer, pointer );
         const totalMetrics = this.getPointerMetrics(firstPointer, pointer );
-        const detail = this.getCustomPointerEventDetail({originalEvent:event,pointer,prevPointer,prevPointer,metrics,totalMetrics})
-        this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
+        const detail = this.getCustomPointerEventDetail({originalEvent:event,pointer,prevPointer,metrics,totalMetrics})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`${eventType}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
 
         // 멀티 포인터 동작
         if(this.pointers.size>1){ this._multiPointerEvent(event) }
+
+        return detail;
     }
 
     // 프로퍼티 이벤트 콜백
@@ -174,6 +218,7 @@ export default class PointerEventHandler{
         const centerY = (pointer1.clientY + pointer2.clientY) / 2;
         const angle = Math.atan2(distanceY, distanceX) * 180 / Math.PI; //degree
         const timeStamp = pointer2.timeStamp;
+        const time = pointer2.time;
         return {
             pointer1,
             pointer2,
@@ -183,7 +228,8 @@ export default class PointerEventHandler{
             distanceY,
             distance,
             angle,
-            timeStamp
+            timeStamp,
+            time,
         }
     }
 
@@ -198,7 +244,8 @@ export default class PointerEventHandler{
         // ----------------------
         // 시간 간격
         // ----------------------
-        const time = (multiPointer2.timeStamp - multiPointer1.timeStamp) / 1000 ;
+        // const time = (multiPointer2.timeStamp - multiPointer1.timeStamp) / 1000 ;
+        const time = (multiPointer2.time - multiPointer1.time) ;
 
         // ----------------------
         // 순간 속도 (프레임 기준)
@@ -261,11 +308,14 @@ export default class PointerEventHandler{
         const detail = this.getCustomPointerEventDetail({originalEvent:event,pointer1,pointer2,multiPointer,prevMultiPointer,firstMultiPointer,metrics,totalMetrics})
         this.target.dispatchEvent(this.getCustomPointerEvent(`multi${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
 
-        if(this.pointers.size==2 && this.maxActivePointers==2){
-            this._gestureEvent(event,multiPointer);
-        }
-
         this.prevMultiPointer = multiPointer;
+
+        if(this.pointers.size==2 && this.maxActivePointers==2){
+            return this._gestureEvent(event,multiPointer);
+        }
+        return detail;
+
+        
     }
 
 
@@ -293,7 +343,7 @@ export default class PointerEventHandler{
             "pointerdown":"gesturestart.peh",
             "pointermove":"gesturechange.peh",
             "pointerup":"gestureend.peh",
-            "pointerleave":"gestureleave.peh",
+            // "pointerleave":"gestureleave.peh",
             "pointercancel":"gesturecancel.peh",
         }?.[event.type];
         if(detalEvent){
@@ -307,6 +357,7 @@ export default class PointerEventHandler{
                 this.target.dispatchEvent(this.getCustomPointerEvent('rotate.peh',{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
             }
         }
+        return detail;
 
     }
 
--- PointerEventHandler.js
+++ PointerEventHandler.js
@@ -1,4 +1,6 @@
 class PointerEventHandler{
+    static VERSION = '2026.01.12'; // 버전 날짜.
+
     pointers = null
     maxActivePointers = 0;
     target = null;
@@ -11,7 +13,7 @@ class PointerEventHandler{
     // interactionThreshold = null; //요소 클릭 후 드래그 캡처 시작 임계값(px)
 
     constructor(target){
-        this.interactionThreshold = {...this.constructor.interactionThreshold}
+        this.interactionThreshold = { ... this.constructor.interactionThreshold }
         
         this.pointers = new Map(); // 멀티터치 제어용
         this.maxActivePointers = 0;
@@ -20,32 +22,32 @@ class PointerEventHandler{
             this.addEventListener(target);
         }
     }
+    addEventListenerOptions = { passive: false }
     addEventListener(target){
         this.target = target
-        const options = { passive: false };
-        target.addEventListener('pointerdown',this.pointerdown);
-        target.addEventListener('pointermove',this.pointermove,options)
+        // const options = { passive: false };
+        this.target.addEventListener('pointerdown',this.pointerdown);
+        this.target.addEventListener('pointermove',this.pointermove,this.addEventListenerOptions)
         
-        target.addEventListener('pointerup',this._pointerend)
-        // target.addEventListener('pointerleave',this._pointerend); // 거의 발생하지 않을꺼다. 발생시 문제가 될 수 있어서 체크 안한다.
-        target.addEventListener('pointercancel',this._pointerend) // 시스템 메세지 등이 뜨면 발생.
-        target.addEventListener('pointercancel',this._pointerend) // 시스템 메세지 등이 뜨면 발생.
+        this.target.addEventListener('pointerup',this._pointerend)
+        // this.target.addEventListener('pointerleave',this._pointerend); // 거의 발생하지 않을꺼다. 발생시 문제가 될 수 있어서 체크 안한다.
+        this.target.addEventListener('pointercancel',this._pointerend) // 시스템 메세지 등이 뜨면 발생.
         
-        target.addEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡쳐
-        target.addEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡쳐
+        this.target.addEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡처
+        this.target.addEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡처
     }
     removeEventListener(){
         if(!this.target) {return; }
-        const options = { passive: false };
+        // const options = { passive: false };
         this.target.removeEventListener('pointerdown',this.pointerdown);
-        this.target.removeEventListener('pointermove',this.pointermove,options)
+        this.target.removeEventListener('pointermove',this.pointermove,this.addEventListenerOptions)
 
         this.target.removeEventListener('pointerup',this._pointerend)
         // this.target.removeEventListener('pointerleave',this._pointerend)
         this.target.removeEventListener('pointercancel',this._pointerend)
         
-        this.target.removeEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡쳐
-        this.target.removeEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡쳐
+        this.target.removeEventListener('gotpointercapture',this.gotpointercapture) // 포인터 캡처
+        this.target.removeEventListener('lostpointercapture',this.lostpointercapture) // 포인터 캡처
     }
     getCustomPointerEventDetail(data){
         return {pointerEventHandler:this, ...data};
@@ -54,17 +56,20 @@ class PointerEventHandler{
         return new CustomEvent(type, { bubbles,cancelable,composed,detail });
     }
     extractPointer({ pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y }) {
-        return { pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp, tangentialPressure,x,y };
+        // timeStamp 값 신뢰하지 마라. 브라우저마다 기준값이 다를 수 있다!!!! time: performance.now() 를 대신 사용함.
+        return { pointerId, pointerType, isPrimary, clientX, clientY, pageX, pageY, screenX, screenY, pressure, tiltX, tiltY, buttons, width, height, twist, timeStamp,time: performance.now(), tangentialPressure,x,y };
     }
 
-    isPointerCaptured = false;
+    get isPointerCaptured(){
+        return this.target.hasPointerCapture(this.firstPointer?.pointerId)
+    }
     gotpointercapture = (event)=>{
-        this.isPointerCaptured = true;
+        // this.isPointerCaptured = true;
         const detail = this.getCustomPointerEventDetail({originalEvent:event})
         this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
     }
     lostpointercapture = (event)=>{
-        this.isPointerCaptured = false;
+        // this.isPointerCaptured = false;
         this._pointerend(event)
     }
 
@@ -96,7 +101,7 @@ class PointerEventHandler{
         this.onpointermove?.(event);
         const detail  = this._pointerEvent(event,pointer) // 포인터 이벤트 처리
 
-        // 캡처를 안했고 드래그가 최소 4px 이 있을 경우만 캡쳐한다.
+        // 캡처를 안했고 드래그 이동이 4~8px 이 있을 경우만 캡처한다.
         if(!this.target.hasPointerCapture(event.pointerId) && Math.abs(detail.totalMetrics.distance) >= (this.interactionThreshold[event.pointerType]??8)){
             this.target.setPointerCapture(event.pointerId); 
         }
@@ -110,6 +115,7 @@ class PointerEventHandler{
         if(this.pointers.has(event.pointerId)){
             this?.[`on${event.type}`]?.(event);
             this._pointerEvent(event,pointer) // 포인터 이벤트 처리
+            this._pointerEvent(event,pointer,'pointerend') // 포인터 end 이벤트 처리
             if(this.target.hasPointerCapture(event.pointerId)) this.target.releasePointerCapture(event.pointerId);
             this.pointers.delete(event.pointerId)
         }
@@ -134,7 +140,8 @@ class PointerEventHandler{
         // ----------------------
         // 시간 간격
         // ----------------------
-        const time = (pointer2.timeStamp - pointer1.timeStamp) / 1000 ;
+        // const time = (pointer2.timeStamp - pointer1.timeStamp) / 1000 ;
+        const time = (pointer2.time - pointer1.time) ;
 
         // ----------------------
         // 순간 속도 (프레임 기준)
@@ -173,13 +180,13 @@ class PointerEventHandler{
             velocityVector,
         };
     }
-    _pointerEvent(event,pointer){
+    _pointerEvent(event,pointer,eventType=event.type){
         const firstPointer = this.firstPointer;
         const prevPointer = this.prevPointer;
         const metrics = this.getPointerMetrics(prevPointer, pointer );
         const totalMetrics = this.getPointerMetrics(firstPointer, pointer );
-        const detail = this.getCustomPointerEventDetail({originalEvent:event,pointer,prevPointer,prevPointer,metrics,totalMetrics})
-        this.target.dispatchEvent(this.getCustomPointerEvent(`${event.type}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
+        const detail = this.getCustomPointerEventDetail({originalEvent:event,pointer,prevPointer,metrics,totalMetrics})
+        this.target.dispatchEvent(this.getCustomPointerEvent(`${eventType}.peh`,{bubbles:event.bubbles,cancelable:event.cancelable,composed:event.composed,detail}));
 
         // 멀티 포인터 동작
         if(this.pointers.size>1){ this._multiPointerEvent(event) }
@@ -211,6 +218,7 @@ class PointerEventHandler{
         const centerY = (pointer1.clientY + pointer2.clientY) / 2;
         const angle = Math.atan2(distanceY, distanceX) * 180 / Math.PI; //degree
         const timeStamp = pointer2.timeStamp;
+        const time = pointer2.time;
         return {
             pointer1,
             pointer2,
@@ -220,7 +228,8 @@ class PointerEventHandler{
             distanceY,
             distance,
             angle,
-            timeStamp
+            timeStamp,
+            time,
         }
     }
 
@@ -235,7 +244,8 @@ class PointerEventHandler{
         // ----------------------
         // 시간 간격
         // ----------------------
-        const time = (multiPointer2.timeStamp - multiPointer1.timeStamp) / 1000 ;
+        // const time = (multiPointer2.timeStamp - multiPointer1.timeStamp) / 1000 ;
+        const time = (multiPointer2.time - multiPointer1.time) ;
 
         // ----------------------
         // 순간 속도 (프레임 기준)
@@ -333,7 +343,7 @@ class PointerEventHandler{
             "pointerdown":"gesturestart.peh",
             "pointermove":"gesturechange.peh",
             "pointerup":"gestureend.peh",
-            "pointerleave":"gestureleave.peh",
+            // "pointerleave":"gestureleave.peh",
             "pointercancel":"gesturecancel.peh",
         }?.[event.type];
         if(detalEvent){
